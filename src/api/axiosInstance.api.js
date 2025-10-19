bdf0aec3fd3365a275a4738b0c0491c994e99f60
Sejun <sam8192@naver.com>
2025-10-19 16:00:07 +0900

    fix:ReDeploy

--- src/api/axiosInstance.api.js
+++ src/api/axiosInstance.api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  "https://port-0-viewmystartup-3-m8ml2ohm3e1c28b1.sel4.cloudtype.app";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
