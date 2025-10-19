b2aeafe0cf66180f8ef31a6a6ff68b3aeeb57528
Sejun <sam8192@naver.com>
2025-10-19 17:15:36 +0900

    feat: Complete frontend deployment setup

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://port-0-viewmystartup-3-m8ml2ohm3e1c28b1.sel4.cloudtype.app',
+      '/api': 'http://localhost:5001',
     },
   },
 });
